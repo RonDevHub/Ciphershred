39480e27209284b5a6beb5fb81250f3ef8875e36
RonDevHub <no_reply@rondev.de>
2026-03-08 13:39:48 +0000

    src/core/Database.php hinzugefügt

---
+++ src/core/Database.php
@@ -0,0 +1,13 @@
+<?php
+class Database {
+    private static $db;
+    public static function getConnection() {
+        if (!self::$db) {
+            self::$db = new PDO('sqlite:/var/www/html/storage/database.sqlite');
+            self::$db->exec("CREATE TABLE IF NOT EXISTS secrets (
+                id TEXT PRIMARY KEY, filename TEXT, expires_at INTEGER, is_file INTEGER
+            )");
+        }
+        return self::$db;
+    }
+}
