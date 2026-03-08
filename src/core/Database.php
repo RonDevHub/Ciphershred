6690ca731359ec34c7e87c33f6a35b027c39146a
RonDevHub <no_reply@rondev.de>
2026-03-08 14:56:32 +0000

    src/core/Database.php aktualisiert

--- src/core/Database.php
+++ src/core/Database.php
@@ -3,7 +3,7 @@ class Database {
     private static $db;
     public static function getConnection() {
         if (!self::$db) {
-            // Pfad absolut zum Container
+            // Absolute Pfadangabe innerhalb des Containers
             self::$db = new PDO('sqlite:/var/www/html/storage/database.sqlite');
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$db->exec("CREATE TABLE IF NOT EXISTS secrets (
